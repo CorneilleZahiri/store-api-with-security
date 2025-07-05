0aa44a8e6530869be01936990ec4c9a637566663
Corneille <cgbeaga@gmail.com>
2025-07-05 17:04:17 +0100

    Initialisation du projet

---
+++ README.md
@@ -0,0 +1,5 @@
+# Développement d'une API de eCommerce
+
+    -  Création des produits
+    -   Création des paniers d'achat
+    -   Ajout de spring security
--- src/main/java/com/store_api/store_api/service/JwtService.java
+++ src/main/java/com/store_api/store_api/service/JwtService.java
@@ -1,23 +1,22 @@
 package com.store_api.store_api.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
-@AllArgsConstructor
 public class JwtService {
-    private final UserService userService;
 
     @Value("${spring.jwt.secret}")
     private String secret;
 
     public String generateToken(String email) {
-        long tokenExpiration = 86400; // 1 day
+        final long tokenExpiration = 86400; // 1 day
 
         return Jwts.builder()
                 .subject(email)
@@ -25,5 +24,28 @@ public class JwtService {
                 .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
                 .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
                 .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Claims claims = getClaims(token);
+
+            return claims.getExpiration().after(new Date());
+
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String getEmailFromToken(String token) {
+        return getClaims(token).getSubject();
     }
 }
